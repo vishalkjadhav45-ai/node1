45898c85857eff9d6adf0928c9ab043a922d360f
vishalkjadhav45-ai <vishalkjadhav45@gmail.com>
2026-01-20 02:30:11 -0500

    Change response message in root route

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('<h1> Codingwale</h1>')
+  res.send('<h1> they dont know me sonn </h1>')
 })
 
 app.listen(port,'0.0.0.0', () => {
