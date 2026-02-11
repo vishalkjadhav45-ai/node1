49eace8e54679a48c9df53f5f6cdac26752a3fa3
vishal jadhav <vishalkjadhav45@gmail.com>
2026-02-11 06:25:58 -0500

    Update response message for root route

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('<h1>done</h1>')
+  res.send('<h1>using pipe node1 deployed successfully</h1>')
 })
 
 app.listen(port,'0.0.0.0', () => {
